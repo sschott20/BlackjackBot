313c89dee70f820e4820305a8a5472ca8b233e1b
Jinwoo Kim <jinwoo.kim@yale.edu>
2023-12-18 16:41:28 -0500

    accounted for aces

--- xmax.py
+++ xmax.py
@@ -5,10 +5,12 @@ def set_probs(deck, probs):
     # Reset probs
     for i in range(1, 11):
         probs[i] = 0
-    
+    total = 0
+
     # Calculate probs
     while deck.size() > 0:
         card = deck.deal(1)[0]
+        total += 1
         if card.rank() >= 10:
             key = 10
         else:
@@ -16,24 +18,24 @@ def set_probs(deck, probs):
         probs[key] += 1
     
     for i in range(1, 11):
-        probs[i] = probs[i]/52
+        probs[i] = probs[i]/total
    
+def get_hit_payoff(dealer_rank, old_rank, new_rank, hit_payoff):
+    total = old_rank + new_rank
+    if total > 21:
+        return -1
+    else:
+        return hit_payoff[dealer_rank][total]
+    
+def get_stand_payoff(player_rank, old_rank, new_rank, stand_payoff):
+    total = old_rank + new_rank
+    if total > 21:
+        return 1
+    else:
+        return stand_payoff[total][player_rank]
 
-def calculate_payoffs(state):
-    def get_hit_payoff(dealer_rank, old_rank, new_rank, hit_payoff):
-        total = old_rank + new_rank
-        if total > 21:
-            return -1
-        else:
-            return hit_payoff[dealer_rank][total]
-        
-    def get_stand_payoff(player_rank, old_rank, new_rank, stand_payoff):
-        total = old_rank + new_rank
-        if total > 21:
-            return 1
-        else:
-            return stand_payoff[total][player_rank]
-        
+
+def calculate_payoffs(state):        
 
     probs = {
         1:0,
@@ -65,7 +67,9 @@ def calculate_payoffs(state):
     
     for i in range(16, -1, -1):
         for j in range(21, -1, -1):
-            for c in range(1, 11):
+            # Account for the dealer hitting an ace
+            stand_payoff[i][j] += probs[1] * min(get_stand_payoff(j, i, 1, stand_payoff), get_stand_payoff(j, i, 11, stand_payoff))
+            for c in range(2, 11):
                 stand_payoff[i][j] += probs[c] * get_stand_payoff(j, i, c, stand_payoff)
     
     # Calculate hit-payoff matrix using above probabilities
@@ -73,10 +77,15 @@ def calculate_payoffs(state):
     action_matrix = [[0 for _ in range(22)] for _ in range(22)]
     # Base cases:
     hit_payoff[21][21] = 0
+    action_matrix[21][21] = "S"
     hit_payoff[20][21] = 1
+    action_matrix[20][21] = "S"
     hit_payoff[19][21] = 1
+    action_matrix[19][21] = "S"
     hit_payoff[18][21] = 1
+    action_matrix[18][21] = "S"
     hit_payoff[17][21] = 1
+    action_matrix[17][21] = "S"
     for i in range(21, 16, -1):
         for j in range(20, -1, -1):
             hit_value = 0
@@ -121,20 +130,24 @@ def xmax_policy():
         hit_payoff, stand_payoff, action_matrix, probs = calculate_payoffs(state)
         actions = state.get_actions()
         # Figure out betting size
-        if len(actions) == 3:
+        if actions[0] != "H":
             heuristic = bet_heuristic(hit_payoff)
             if heuristic < 0:
                 return 1
-            elif heuristic > 0.02:
+            elif heuristic > 0.3:
                 return 7
             else:
                 return 4
         # Figure out best move
         else:
-            print(state)
-            print(stand_payoff)
-            print(action_matrix[16][21])
-            input()
+            move_payoff = get_hit_payoff(state.dealer_score(), state.player_score(), 0, hit_payoff)
+            # Check for aces
+            sorted_hand = sorted(state.player_hand)
+            if sorted_hand[0].rank() == 1:
+                ace_move_payoff = get_hit_payoff(state.dealer_score(), state.player_score(), 10, hit_payoff)
+                if ace_move_payoff > move_payoff:
+                    return action_matrix[state.dealer_score()][state.player_score()+10]
+
             return action_matrix[state.dealer_score()][state.player_score()]
     
     return best_move
